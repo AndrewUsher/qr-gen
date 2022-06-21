9fafe595786f49c9e26fe025e88b5079aadaaec0
Andrew Usher <andrewusher00@gmail.com>
2022-06-21 07:03:10 -0500

    feat: add initial styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,10 +19,12 @@
   }
 </script>
 
-<h1>Welcome to QRGen!</h1>
-<p>Enter a URL below to generate a QR code</p>
-<input type="text" placeholder="Enter URL Here" bind:value={url} />
-<button on:click={submitURL}>Generate QR Code</button>
-{#if qrCodeDataURL}
-  <img src={qrCodeDataURL} alt={`QR Code for ${url}`} />
-{/if}
+<main class="sha shadow-lg bg-white p-8 rounded-md">
+  <h1 class="text-center font-bold text-2xl mb-1">Welcome to QRGen!</h1>
+  <p class="mb-4 text-center">Enter a URL below to generate a QR code</p>
+  <input class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white w-60" placeholder="Enter URL Here" bind:value={url} />
+	<button class="px-8 rounded-r-lg bg-blue-400  text-gray-800 font-bold p-4 uppercase border-blue-500 border-t border-b border-r" on:click={submitURL}>Generate QR Code</button>
+  {#if qrCodeDataURL}
+    <img class="m-auto w-60 h-60" src={qrCodeDataURL} alt={`QR Code for ${url}`} />
+  {/if}
+</main>
